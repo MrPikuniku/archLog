120d8fcc6eda9bb843fe1208e9049aa19e2a0a9c
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2024-04-09 13:05:01 +0200

    installing PHP CS Fixer

---
+++ src/Controller/MainController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MainController
+{
+    #[Route]
+    public function HomePage(): Response
+    {
+        return  new Response("<strong>Hello World!</strong>");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
